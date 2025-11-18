6cdb6377969eeaea6bdb89e1abbd179d4bcb9f29
Herwinstenil <herwinstenil24@gmail.com>
2025-11-18 11:24:59 +0530

    Appointment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './Pages/Navbar'
-import Footer from './Pages/Footer'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
