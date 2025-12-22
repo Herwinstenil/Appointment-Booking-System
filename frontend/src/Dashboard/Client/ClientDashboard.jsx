bf69d23d614b1c21e868ce29c4e899fa461627ff
Herwinstenil <herwinstenil24@gmail.com>
2025-12-22 22:36:02 +0530

    Appointment

--- frontend/src/Dashboard/Client/ClientDashboard.jsx
+++ frontend/src/Dashboard/Client/ClientDashboard.jsx
@@ -4180,6 +4180,14 @@ const ClientDashboard = () => {
                                         )}
                                     </div>
                                 </div>
+                                  <div>
+                        <label className="block text-sm font-semibold text-gray-700 mb-2">
+                            Role
+                        </label>
+                        <div className="w-full px-4 py-3 border-2 border-gray-200 rounded-xl  text-gray-700">
+                            Customer
+                        </div>
+                    </div>
                                 <div>
                                     <label className="block text-sm font-semibold text-gray-700 mb-2">
                                         Status
