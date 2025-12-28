3bb504bc3d4f181da75bd8f913fbdae6a72f2fc7
Herwinstenil <herwinstenil24@gmail.com>
2025-12-28 20:33:22 +0530

    Appointment

--- frontend/src/Dashboard/User/UserDashboard.jsx
+++ frontend/src/Dashboard/User/UserDashboard.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import {
     BookOpen,
     History,
