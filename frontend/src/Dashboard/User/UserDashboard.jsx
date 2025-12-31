a3be62028ce6141606f044d4a160c2dd398e53d1
Herwinstenil <herwinstenil24@gmail.com>
2025-12-31 19:25:44 +0530

    Appointment

--- frontend/src/Dashboard/User/UserDashboard.jsx
+++ frontend/src/Dashboard/User/UserDashboard.jsx
@@ -2721,7 +2721,8 @@ const UserDashboard = () => {
             {/* Rating Modal */}
             {showRatingModal && <RatingModal />}
 
-            
+             {/* Rating Modal */}
+            {showRatingModal && <RatingModal />}
 
             {/* Booking Success Notification */}
             {bookingSuccess && (
