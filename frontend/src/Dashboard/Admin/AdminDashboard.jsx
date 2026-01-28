9c0909e8b429b01fed5a25822263ec5669c4f480
Herwinstenil <herwinstenil24@gmail.com>
2026-01-28 17:03:32 +0530

    Appointment

--- frontend/src/Dashboard/Admin/AdminDashboard.jsx
+++ frontend/src/Dashboard/Admin/AdminDashboard.jsx
@@ -3951,7 +3951,7 @@ const AdminDashboard = () => {
                                                 />
                                             </th>
                                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">User</th>
-                                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Role</th>
+                                            <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Role</th>
                                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
                                             <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Client No</th>
                                             <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Revenue</th>
